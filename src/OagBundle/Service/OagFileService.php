26062fc2983ec17f2b5b77e4dc42291a2040b9ef
Toby Batch <tobias@neontribe.co.uk>
2017-08-15 16:29:04 +0100

    Added a mechanism to prevnt filename clashes

--- src/OagBundle/Service/OagFileService.php
+++ src/OagBundle/Service/OagFileService.php
@@ -6,22 +6,9 @@ class OagFileService extends AbstractService {
 
     public function getXMLFileName($oagFile) {
         $filename = $oagFile->getDocumentName();
-        $ext = pathinfo($filename, PATHINFO_EXTENSION);
-        if ($ext != 'xml') {
-            $filename .= '.xml';
-        }
-        return $filename;
+        $name = pathinfo($filename, PATHINFO_FILENAME);
 
-
-//        $output_array = [];
-//        $re = '/[^\\\\\/]+(?=\.[\w]+$)|[^\\\\\/]+$/';
-//        preg_match($re, $filename, $output_array, PREG_OFFSET_CAPTURE, 0);
-//
-//        if (count($output_array)) {
-//            $rawname = $output_array[0][0];
-//        } else {
-//            $rawname = uniqid('file');
-//        }
+        return $name . '.' . date("Ymd_His") . '.xml';
     }
 
 }
