300088596c642d04ef4818a3f9f601cce56a70c4
Toby Batch <tobias@neontribe.co.uk>
2017-08-15 11:43:04 +0100

    Fixed (very basic) tests

--- src/OagBundle/Tests/Controller/ActivityControllerTest.php
+++ src/OagBundle/Tests/Controller/ActivityControllerTest.php
@@ -5,26 +5,23 @@ namespace OagBundle\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ActivityControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/index');
-    }
+ {
 
     public function testEnhance()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/enhance');
+        $crawler = $client->request('GET', '/activity/enhance/5/AB123');
+        $code = $client->getResponse()->getStatusCode();
+        $this->assertTrue($code >= 200 && $code <= 399);
     }
 
-    public function testMerge()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/merge');
-    }
-
+//    public function testMerge()
+//    {
+//        $client = static::createClient();
+//
+//        $crawler = $client->request('GET', '/activity/merge');
+//        $code = $client->getResponse()->getStatusCode();
+//        $this->assertTrue($code >= 200 && $code <= 399);
+//    }
 }
