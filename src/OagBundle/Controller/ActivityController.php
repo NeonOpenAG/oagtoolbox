601e6a69e3e073f43ad9d2fa1fa4c8a914e1a926
Harry <me@hoolean.com>
2017-08-15 09:16:37 +0100

    Rudimentary merge interface form generation

--- src/OagBundle/Controller/ActivityController.php
+++ src/OagBundle/Controller/ActivityController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use OagBundle\Entity\OagFile;
 use OagBundle\Service\ActivityService;
 
@@ -30,7 +31,55 @@ class ActivityController extends Controller
         $root = $srvActivity->load($file);
         $activity = $srvActivity->getActivityById($root, $iatiActivityId);
 
-        return [];
+        # build the form
+        $formBuilder = $this->createFormBuilder(array(), array());
+
+        # current sectors
+        $current = $srvActivity->getActivitySectors($activity);
+        $formBuilder->add('current', ChoiceType::class, array(
+            'expanded' => true,
+            'multiple' => true,
+            'choices' => array_column($current, 'code', 'description')
+        ));
+
+
+        # suggested sectors
+        $suggested = array();
+        foreach ($file->getSectors() as $sector) {
+            # if it's not from our activity, ignore it
+            if ($sector->getId() !== $iatiActivityId) {
+                continue;
+            }
+            $suggested[] = $sector;
+        }
+        $formBuilder->add('suggested', ChoiceType::class, array(
+            'expanded' => true,
+            'multiple' => true,
+            'choices' => array_reduce($suggested, function ($result, $item) {
+                $result[$item->getCode()] = $item->getId();
+                return $result;
+            }, array())
+        ));
+
+        foreach ($file->getEnhancingDocuments() as $otherFile) {
+            $name = $otherFile->getDocumentName();
+            $sectors = $otherFile->getSectors();
+            $id = $otherFile->getId();
+
+            $formBuilder->add("enhanced_$id", ChoiceType::class, array(
+                'expanded' => true,
+                'multiple' => true,
+                'label' => $name,
+                'choices' => array_reduce($sectors->toArray(), function ($result, $item) {
+                    $result[$item->getCode()] = $item->getId();
+                    return $result;
+                }, array())
+            ));
+        }
+
+        return array(
+            'form' => $formBuilder->getForm()->createView()
+        );
     }
 
     /**
