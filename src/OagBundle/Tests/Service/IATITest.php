a8e18b9c9d89b7a610b543c522a33842f46b19d3
Harry <me@hoolean.com>
2017-09-04 10:21:43 +0100

    Make service test indentation consistent

--- src/OagBundle/Tests/Service/IATITest.php
+++ src/OagBundle/Tests/Service/IATITest.php
@@ -7,258 +7,258 @@ use Symfony\Bundle\WebProfilerBundle\Tests\TestCase;
 
 class IATITest extends TestCase {
 
-  protected $container;
+    protected $container;
 
-  /**
-   * @var \Doctrine\Common\Persistence\ObjectManager
-   */
-  protected $em;
+    /**
+    * @var \Doctrine\Common\Persistence\ObjectManager
+    */
+    protected $em;
 
-  /**
-   * @var OagFileService
-   */
-  protected $oagFileService;
-  protected $testOagFile;
+    /**
+    * @var OagFileService
+    */
+    protected $oagFileService;
+    protected $testOagFile;
 
-  public function setUp() {
-    $kernel = new \AppKernel("test", true);
-    $kernel->boot();
-    $this->container = $kernel->getContainer();
-    $this->em = $this->container
-        ->get('doctrine')
-        ->getManager();
+    public function setUp() {
+        $kernel = new \AppKernel("test", true);
+        $kernel->boot();
+        $this->container = $kernel->getContainer();
+        $this->em = $this->container
+            ->get('doctrine')
+            ->getManager();
 
-    // Find one OagFile::IATI_DOCUMENT to test with.
-    $this->testOagFile = $this->em
-        ->getRepository(OagFile::class)
-        ->findOneBy(array('fileType' => OagFile::OAGFILE_IATI_DOCUMENT));
+        // Find one OagFile::IATI_DOCUMENT to test with.
+        $this->testOagFile = $this->em
+            ->getRepository(OagFile::class)
+            ->findOneBy(array('fileType' => OagFile::OAGFILE_IATI_DOCUMENT));
 
-    $this->oagFileService = $this->container->get(OagFileService::class);
-    $this->oagFileService->setContainer($this->container);
-  }
-
-  public function testLoad() {
-    $srvIATI = $this->container->get(IATI::class);
-    $srvIATI->setContainer($this->container);
-
-    $loadedFile = $srvIATI->load($this->testOagFile);
-    $this->assertInstanceOf('SimpleXMLElement', $loadedFile);
-  }
-
-  public function testXpathNS() {
-    $namespaceUri =  $this->container->getParameter('classifier')['namespace_uri'];
-
-    $srvIATI = $this->container->get(IATI::class);
-    $srvIATI->setContainer($this->container);
-
-    $activity = $srvIATI->load($this->testOagFile)
-        ->xpath('/iati-activities/iati-activity')[0];
-
-    # Add a child tag for test purposes.
-    $tag = $activity->addChild('openag:tag', '', $namespaceUri);
-    $tags = $srvIATI->xpathNS($activity, './openag:tag');
-    $this->assertEquals(
-        [$tag],
-        $tags,
-        'Check that namespaced tag is found correctly.'
-    );
-  }
-
-  public function testParseXML() {
-    $srvIATI = $this->container->get(IATI::class);
-    $srvIATI->setContainer($this->container);
-
-    $iatiFileString = $this->oagFileService->getContents($this->testOagFile);
-
-    $root = $srvIATI->parseXML($iatiFileString);
-    $this->assertInstanceOf('SimpleXMLElement', $root);
-
-    # Ensure that the namspace was added to each activity.
-    $activities = $root->xpath('/iati-activities/iati-activity');
-    foreach ($activities as $activity) {
-      $activityDocAttributes = $activity->attributes();
-      $this->assertObjectHasAttribute(
-          'xmlns:openag',
-          $activityDocAttributes,
-          'Activities have openag namespace.'
-      );
+        $this->oagFileService = $this->container->get(OagFileService::class);
+        $this->oagFileService->setContainer($this->container);
     }
 
-    # Check that the method logs when expected.
+    public function testLoad() {
+        $srvIATI = $this->container->get(IATI::class);
+        $srvIATI->setContainer($this->container);
 
-    $srvIATI->parseXML('');
-  }
+        $loadedFile = $srvIATI->load($this->testOagFile);
+        $this->assertInstanceOf('SimpleXMLElement', $loadedFile);
+    }
 
-  public function testToXML() {
-      $srvIATI = $this->container->get(IATI::class);
-      $srvIATI->setContainer($this->container);
+    public function testXpathNS() {
+        $namespaceUri =  $this->container->getParameter('classifier')['namespace_uri'];
 
-      $iatiFileString = $this->oagFileService->getContents($this->testOagFile);
+        $srvIATI = $this->container->get(IATI::class);
+        $srvIATI->setContainer($this->container);
 
-      $root = $srvIATI->parseXML($iatiFileString);
-      $toXML = $srvIATI->toXML($root);
-      $this->assertXmlStringEqualsXmlString($root->asXML(), $toXML);
-  }
+        $activity = $srvIATI->load($this->testOagFile)
+            ->xpath('/iati-activities/iati-activity')[0];
 
-  public function testSummariseActivityToArray() {
-      $srvIATI = $this->getMockBuilder(IATI::class)
-          ->setMethods(array(
-              'getActivityId',
-              'getActivityTitle',
-              'getActivityTags',
-              'getActivityLocations'
-          ))
-          ->getMock();
-      $srvIATI->setContainer($this->container);
+        # Add a child tag for test purposes.
+        $tag = $activity->addChild('openag:tag', '', $namespaceUri);
+        $tags = $srvIATI->xpathNS($activity, './openag:tag');
+        $this->assertEquals(
+            [$tag],
+            $tags,
+            'Check that namespaced tag is found correctly.'
+        );
+    }
 
-      # Load a single activity for test purposes.
-      $activity = $srvIATI->load($this->testOagFile)
-          ->xpath('/iati-activities/iati-activity')[0];
+    public function testParseXML() {
+        $srvIATI = $this->container->get(IATI::class);
+        $srvIATI->setContainer($this->container);
 
-      # Assert that the methods required to build the summary array are called.
-      $srvIATI->expects($this->once())
-          ->method('getActivityId');
-      $srvIATI->expects($this->once())
-          ->method('getActivityTitle');
-      $srvIATI->expects($this->once())
-          ->method('getActivityTags');
-      $srvIATI->expects($this->once())
-          ->method('getActivityLocations');
+        $iatiFileString = $this->oagFileService->getContents($this->testOagFile);
 
-      # Assert that the correct keys are used in the summary array.
-      $summarisedActivity = $srvIATI->summariseActivityToArray($activity);
-      $this->assertArrayHasKey('id', $summarisedActivity);
-      $this->assertArrayHasKey('name', $summarisedActivity);
-      $this->assertArrayHasKey('tags', $summarisedActivity);
-      $this->assertArrayHasKey('locations', $summarisedActivity);
-  }
+        $root = $srvIATI->parseXML($iatiFileString);
+        $this->assertInstanceOf('SimpleXMLElement', $root);
 
-  public function testSummariseToArray() {
-      $srvIATI = $this->getMockBuilder(IATI::class)
-          ->setMethods(array('summariseActivityToArray'))
-          ->getMock();
-      $srvIATI->setContainer($this->container);
-      $root = $srvIATI->load($this->testOagFile);
-      $activities = $root
-          ->xpath('/iati-activities/iati-activity');
+        # Ensure that the namspace was added to each activity.
+        $activities = $root->xpath('/iati-activities/iati-activity');
+        foreach ($activities as $activity) {
+            $activityDocAttributes = $activity->attributes();
+            $this->assertObjectHasAttribute(
+                'xmlns:openag',
+                $activityDocAttributes,
+                'Activities have openag namespace.'
+            );
+        }
 
-      # Assert that the methods required to build the summary array are called.
-      $srvIATI->expects($this->exactly(count($activities)))
-          ->method('summariseActivityToArray');
+        # Check that the method logs when expected.
 
-      $summary = $srvIATI->summariseToArray($root);
-      $this->assertEquals(count($activities), count($summary));
-  }
+        $srvIATI->parseXML('');
+    }
 
-  // Not sure if we should unit test fixture data methods.
-  public function testGetFixtureData() {}
+    public function testToXML() {
+        $srvIATI = $this->container->get(IATI::class);
+        $srvIATI->setContainer($this->container);
 
-  public function testGetActivities() {
-      $srvIATI = $this->container->get(IATI::class);
-      $srvIATI->setContainer($this->container);
-      $mock = $this
-          ->getMockBuilder('Traversable')
-          ->setMethods(['xpath', 'key', 'next', 'valid', 'rewind', 'current'])
-          ->getMock();
+        $iatiFileString = $this->oagFileService->getContents($this->testOagFile);
 
-      # We expect the xpath method to be ran once with the expected param.
-      $expectedParam = '/iati-activities/iati-activity';
-      $mock->expects($this->once())
-          ->method('xpath')
-          ->with($expectedParam)
-          ->willReturn([0]);
+        $root = $srvIATI->parseXML($iatiFileString);
+        $toXML = $srvIATI->toXML($root);
+        $this->assertXmlStringEqualsXmlString($root->asXML(), $toXML);
+    }
 
-      $activities = $srvIATI->getActivities($mock);
+    public function testSummariseActivityToArray() {
+        $srvIATI = $this->getMockBuilder(IATI::class)
+            ->setMethods(array(
+                'getActivityId',
+                'getActivityTitle',
+                'getActivityTags',
+                'getActivityLocations'
+            ))
+            ->getMock();
+        $srvIATI->setContainer($this->container);
 
-      # Assert that the result of xpath is directly returned without modification.
-      $this->assertNotEmpty($activities);
-  }
+        # Load a single activity for test purposes.
+        $activity = $srvIATI->load($this->testOagFile)
+            ->xpath('/iati-activities/iati-activity')[0];
 
-  public function testGetActivityById() {
-      $srvIATI = $this->getMockBuilder(IATI::class)
-          ->setMethods(array('getActivities', 'getActivityId'))
-          ->enableProxyingToOriginalMethods()
-          ->getMock();
-      $srvIATI->setContainer($this->container);
+        # Assert that the methods required to build the summary array are called.
+        $srvIATI->expects($this->once())
+            ->method('getActivityId');
+        $srvIATI->expects($this->once())
+            ->method('getActivityTitle');
+        $srvIATI->expects($this->once())
+            ->method('getActivityTags');
+        $srvIATI->expects($this->once())
+            ->method('getActivityLocations');
 
+        # Assert that the correct keys are used in the summary array.
+        $summarisedActivity = $srvIATI->summariseActivityToArray($activity);
+        $this->assertArrayHasKey('id', $summarisedActivity);
+        $this->assertArrayHasKey('name', $summarisedActivity);
+        $this->assertArrayHasKey('tags', $summarisedActivity);
+        $this->assertArrayHasKey('locations', $summarisedActivity);
+    }
 
-      $iatiFileString = $this->oagFileService->getContents($this->testOagFile);
+    public function testSummariseToArray() {
+        $srvIATI = $this->getMockBuilder(IATI::class)
+            ->setMethods(array('summariseActivityToArray'))
+            ->getMock();
+        $srvIATI->setContainer($this->container);
+        $root = $srvIATI->load($this->testOagFile);
+        $activities = $root
+            ->xpath('/iati-activities/iati-activity');
 
-      $root = $srvIATI->parseXML($iatiFileString);
+        # Assert that the methods required to build the summary array are called.
+        $srvIATI->expects($this->exactly(count($activities)))
+            ->method('summariseActivityToArray');
 
-      # Load a single activity for test purposes.
-      $activities = $root->xpath('/iati-activities/iati-activity');
+        $summary = $srvIATI->summariseToArray($root);
+        $this->assertEquals(count($activities), count($summary));
+    }
 
-      $activityToCheck = $activities[0];
-      $activityIdToCheck = (string) $activityToCheck->xpath('./iati-identifier')[0];
+    // Not sure if we should unit test fixture data methods.
+    public function testGetFixtureData() {}
 
-      # Assert that the methods required to find the activity ID are called.
-      $srvIATI->expects($this->exactly(2))
-          ->method('getActivities')
-          ->willReturn($activities);
+    public function testGetActivities() {
+        $srvIATI = $this->container->get(IATI::class);
+        $srvIATI->setContainer($this->container);
+        $mock = $this
+            ->getMockBuilder('Traversable')
+            ->setMethods(['xpath', 'key', 'next', 'valid', 'rewind', 'current'])
+            ->getMock();
 
-      # getActivityId will only be called once due to it being first in the list.
-      $srvIATI->expects($this->exactly(count($activities) + 1))
-          ->method('getActivityId')
-          ->willReturn($activityIdToCheck);
+        # We expect the xpath method to be ran once with the expected param.
+        $expectedParam = '/iati-activities/iati-activity';
+        $mock->expects($this->once())
+            ->method('xpath')
+            ->with($expectedParam)
+            ->willReturn([0]);
 
-      # Provide a valid ID and check to see if it's the activity we expect.
-      $returnedActivity = $srvIATI->getActivityById($root, $activityIdToCheck);
-      $this->assertEquals(
-          $activityToCheck,
-          $returnedActivity,
-          'We receive the expected activity corresponding to the provided id.'
-      );
+        $activities = $srvIATI->getActivities($mock);
 
-      # Provide an invalid ID to check the return value.
-      $returnedActivity = $srvIATI->getActivityById($root, '');
-      $this->assertEmpty($returnedActivity);
+        # Assert that the result of xpath is directly returned without modification.
+        $this->assertNotEmpty($activities);
+    }
 
-  }
-
-  public function testGetActivityId() {
-      $srvIATI = $this->container->get(IATI::class);
-      $srvIATI->setContainer($this->container);
-      $mock = $this
-          ->getMockBuilder('Traversable')
-          ->setMethods(['xpath', 'key', 'next', 'valid', 'rewind', 'current'])
-          ->getMock();
-
-      # We expect the xpath method to be ran once with the expected param.
-      # It will return the two choices.
-      $expectedParam = './iati-identifier';
-      $mock->expects($this->once())
-          ->method('xpath')
-          ->with($expectedParam)
-          ->willReturn([0, 1]);
-
-      $activities = $srvIATI->getActivityId($mock);
-
-      # Assert that the first item found by the xpath is returned.
-      $this->assertEquals(0, $activities);
-
-  }
-
-  public function testGetActivityTitle() {
-
-  }
+    public function testGetActivityById() {
+        $srvIATI = $this->getMockBuilder(IATI::class)
+            ->setMethods(array('getActivities', 'getActivityId'))
+            ->enableProxyingToOriginalMethods()
+            ->getMock();
+        $srvIATI->setContainer($this->container);
 
 
-  public function testGetActivityMapData() {}
-  public function testGetActivityTags() {}
-  public function testGetActivityLocations() {}
-  public function testAddActivityTag() {}
-  public function testAddActivityLocation() {}
-  public function testRemoveActivityTag() {}
-  public function testRemoveActivityLocation() {}
+        $iatiFileString = $this->oagFileService->getContents($this->testOagFile);
 
-  /**
-   * {@inheritDoc}
-   */
-  protected function tearDown() {
-    parent::tearDown();
+        $root = $srvIATI->parseXML($iatiFileString);
 
-    $this->em->close();
-    $this->em = null; // avoid memory leaks
-  }
+        # Load a single activity for test purposes.
+        $activities = $root->xpath('/iati-activities/iati-activity');
+
+        $activityToCheck = $activities[0];
+        $activityIdToCheck = (string) $activityToCheck->xpath('./iati-identifier')[0];
+
+        # Assert that the methods required to find the activity ID are called.
+        $srvIATI->expects($this->exactly(2))
+            ->method('getActivities')
+            ->willReturn($activities);
+
+        # getActivityId will only be called once due to it being first in the list.
+        $srvIATI->expects($this->exactly(count($activities) + 1))
+            ->method('getActivityId')
+            ->willReturn($activityIdToCheck);
+
+        # Provide a valid ID and check to see if it's the activity we expect.
+        $returnedActivity = $srvIATI->getActivityById($root, $activityIdToCheck);
+        $this->assertEquals(
+            $activityToCheck,
+            $returnedActivity,
+            'We receive the expected activity corresponding to the provided id.'
+        );
+
+        # Provide an invalid ID to check the return value.
+        $returnedActivity = $srvIATI->getActivityById($root, '');
+        $this->assertEmpty($returnedActivity);
+
+    }
+
+    public function testGetActivityId() {
+        $srvIATI = $this->container->get(IATI::class);
+        $srvIATI->setContainer($this->container);
+        $mock = $this
+            ->getMockBuilder('Traversable')
+            ->setMethods(['xpath', 'key', 'next', 'valid', 'rewind', 'current'])
+            ->getMock();
+
+        # We expect the xpath method to be ran once with the expected param.
+        # It will return the two choices.
+        $expectedParam = './iati-identifier';
+        $mock->expects($this->once())
+            ->method('xpath')
+            ->with($expectedParam)
+            ->willReturn([0, 1]);
+
+        $activities = $srvIATI->getActivityId($mock);
+
+        # Assert that the first item found by the xpath is returned.
+        $this->assertEquals(0, $activities);
+
+    }
+
+    public function testGetActivityTitle() {
+
+    }
+
+
+    public function testGetActivityMapData() {}
+    public function testGetActivityTags() {}
+    public function testGetActivityLocations() {}
+    public function testAddActivityTag() {}
+    public function testAddActivityLocation() {}
+    public function testRemoveActivityTag() {}
+    public function testRemoveActivityLocation() {}
+
+    /**
+    * {@inheritDoc}
+    */
+    protected function tearDown() {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
 }
