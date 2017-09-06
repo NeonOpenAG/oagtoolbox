699aa9ece80adc3c261a77db667f3b176b13b647
Toby Batch <tobias@neontribe.co.uk>
2017-09-06 13:14:47 +0100

    Added JS for file uploads

--- src/OagBundle/Controller/WireframeController.php
+++ src/OagBundle/Controller/WireframeController.php
@@ -2,20 +2,49 @@
 
 namespace OagBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use OagBundle\Entity\OagFile;
+use OagBundle\Form\OagFileType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
  * @Route("/wireframe")
  * @Template
  */
 class WireframeController extends Controller
-{
+ {
+
     /**
-     * @Route("/upload")
+     * @Route("/")
      */
-    public function uploadAction()
-    {
+    public function indexAction() {
+        return array();
+    }
+
+    /**
+     * @Route("/uploadIati")
+     */
+    public function uploadIatiAction() {
+        $oagfile = new OagFile();
+        $oagfile->setFileType(OagFile::OAGFILE_IATI_SOURCE_DOCUMENT);
+        $sourceUploadForm = $this->createForm(OagFileType::class, $oagfile);
+        $sourceUploadForm->add('Upload', SubmitType::class, array(
+            'attr' => array('class' => 'submit'),
+        ));
+
+        $data = array(
+            'source_upload_form' => $sourceUploadForm->createView()
+        );
+
+        return $data;
+    }
+
+    /**
+     * @Route("/uploadEnhancement")
+     */
+    public function uploadEnhancementAction() {
         return array();
     }
 
