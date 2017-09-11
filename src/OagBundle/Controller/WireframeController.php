530cdbf06b10b35c192f1b93277affb46d3019e7
Harry Dalton <alerts@hoolean.com>
2017-09-11 11:50:38 +0100

    Oag 91 classifier suggestions (#64)
    * Correct wireframe template names
    
    * Basic tag suggestions form is now rendered
    
    * Add functionality to tag suggestion form
    
    * Added back buttons where appropriate

--- src/OagBundle/Controller/WireframeController.php
+++ src/OagBundle/Controller/WireframeController.php
@@ -13,6 +13,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -161,12 +162,101 @@ class WireframeController extends Controller {
      * @Route("/classifier/{id}/{activityId}")
      * @ParamConverter("file", class="OagBundle:OagFile")
      */
-    public function classifierSuggestionAction(OagFile $file, $activityId) {
+    public function classifierSuggestionAction(Request $request, OagFile $file, $activityId) {
         if (!$file->hasFileType(OagFile::OAGFILE_IATI_DOCUMENT)) {
             // TODO throw a reasonable error
         }
 
-        return array();
+        $em = $this->getDoctrine()->getManager();
+        $srvOagFile = $this->get(OagFileService::class);
+        $srvIATI = $this->get(IATI::class);
+
+        $root = $srvIATI->load($file);
+        $activity = $srvIATI->getActivityById($root, $activityId);
+
+        if (is_null($activity)) {
+            // TODO throw a reasonable error
+        }
+
+        $currentTags = $srvIATI->getActivityTags($activity);
+        $suggestedTags = array();
+        foreach ($file->getSuggestedTags() as $sugTag) {
+            if (in_array($sugTag, $suggestedTags) || in_array($sugTag->getTag(), $currentTags)) {
+                // no duplicates
+                continue;
+            }
+
+            if ((!is_null($sugTag->getActivityId())) && $sugTag->getActivityId() !== $activityId) {
+                // only those generic else specific to this activity
+                continue;
+            }
+
+            $suggestedTags[] = $sugTag->getTag();
+        }
+        $allTags = array_merge($currentTags, $suggestedTags);
+
+        $form = $this->createFormBuilder()
+            ->add('tags', ChoiceType::class, array(
+                'expanded' => true,
+                'multiple' => true,
+                'choices' => $allTags,
+                'data' => $currentTags, // default current tags to ticked
+                'choice_label' => function ($value, $key, $index) {
+                    $desc = $value->getDescription();
+                    $vocab = $value->getVocabulary();
+                    return "$desc ($vocab)";
+                }
+            ))
+            ->add('back', SubmitType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('back')->isClicked()) {
+                return $this->redirect($this->generateUrl('oag_wireframe_classifier', array('id' => $file->getId()))); 
+            }
+
+            $editedTags = $form->getData()['tags'];
+
+            // have any current tags been removed
+            $toRemove = array();
+            foreach ($currentTags as $currentTag) {
+                if (!in_array($currentTag, $editedTags)) {
+                    $srvIATI->removeActivityTag($activity, $currentTag);
+                    $toRemove[] = $currentTag;
+                }
+            }
+
+            // have any suggsted tags been added
+            $toAdd = array();
+            foreach ($suggestedTags as $suggestedTag) {
+                if (in_array($suggestedTag, $editedTags)) {
+                    $srvIATI->addActivityTag($activity, $suggestedTag);
+                    $toAdd[] = $suggestedTag;
+                }
+            }
+
+            $resultXML = $srvIATI->toXML($root);
+            $srvOagFile->setContents($file, $resultXML);
+
+            $change = new Change();
+            $change->setAddedTags($toAdd);
+            $change->setRemovedTags($toRemove);
+            $change->setFile($file);
+            $change->setActivityId($activityId);
+            $change->setTimestamp(new \DateTime('now'));
+            $em->persist($change);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('oag_wireframe_classifier', array('id' => $file->getId()))); 
+        }
+
+        return array(
+            'file' => $file,
+            'activity' => $srvIATI->summariseActivityToArray($activity),
+            'form' => $form->createView()
+        );
     }
 
     /**
