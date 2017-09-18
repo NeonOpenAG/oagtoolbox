13a69df3fcc1a459751cbedecef31b04053cacf7
Harry Dalton <alerts@hoolean.com>
2017-09-18 11:26:39 +0100

    Oag 129 tsv geocoder (#79)
    * Transition to new TSV Geocoder fixture data and modify Geolocation entity and related code as appropriate
    
    * Remove Geocoder testing action
    
    * Remove call to removed location method from IATIService
    
    * Correct tests following removal of methods

--- src/OagBundle/Service/Geocoder.php
+++ src/OagBundle/Service/Geocoder.php
@@ -4,26 +4,27 @@ namespace OagBundle\Service;
 
 use OagBundle\Entity\Geolocation;
 use OagBundle\Entity\OagFile;
+use OagBundle\Service\CSV;
 
 class Geocoder extends AbstractAutoService {
 
     public function processUri($sometext) {
         // TODO implement non-fixture process
-        return json_decode($this->getFixtureData(), true);
+        $csvService = $this->getContainer()->get(CSV::class);
+        return $csvService->toArray($this->getFixtureData(), "\t");
     }
 
     public function processString($sometext) {
         // TODO implement non-fixture process
-        $uri = $this->getUri();
-
-        $json = $this->getFixtureData();
-
-        return json_decode($json, true);
+        $csvService = $this->getContainer()->get(CSV::class);
+        return $csvService->toArray($this->getFixtureData(), "\t");
     }
 
     public function processXML($contents) {
         // TODO implement non-fixture process
-        return json_decode($this->getFixtureData(), true);
+        // TODO how to do this per-activity?
+        $csvService = $this->getContainer()->get(CSV::class);
+        return $csvService->toArray($this->getFixtureData(), "\t");
     }
 
     public function getName() {
@@ -32,7 +33,7 @@ class Geocoder extends AbstractAutoService {
 
     public function getFixtureData() {
         $kernel = $this->getContainer()->get('kernel');
-        $path = $kernel->locateResource('@OagBundle/Resources/fixtures/geocoder.json');
+        $path = $kernel->locateResource('@OagBundle/Resources/fixtures/geocoder.tsv');
         $contents = file_get_contents($path);
 
         return $contents;
@@ -49,76 +50,41 @@ class Geocoder extends AbstractAutoService {
      * @param OagFile $file the file to process
      */
     public function geocodeOagFile(OagFile $file) {
-        $json = $this->processOagFile($file);
-        $results = json_decode($json, true);
+        $em = $this->get('doctrine')->getManager();
+        $geolocRepo = $this->get('doctrine')->getRepository(Geolocation::class);
+        $srvOagFile = $this->getContainer()->get(OagFileService::class);
+
+        $xml = $srvOagFile->getContents($file);
+        $locations = $this->processXML($xml);
 
         $file->clearGeolocations();
-        $em = $this->getContainer()->get('doctrine')->getManager();
-        $geolocationrepo = $this->getContainer()->get('doctrine')->getRepository(Geolocation::class);
 
-        foreach ($results as $activity) {
-            $iatiActivityId = $activity['project_id'] ?? null;
-            $locations = $activity['locations'];
-            foreach ($locations as $location) {
-                $locationId = $location['id'];
-                $vocabId = '99'; // TODO get a valif vocab id
-                // Does this location already exist for this IATI ID?
-                $geolocation = $geolocationrepo->findOneBy(
-                    array(
-                        'iatiActivityId' => $iatiActivityId,
-                        'geolocationId' => $locationId,
-                        'vocabId' => $vocabId,
-                    )
-                );
+        foreach ($locations as $location) {
+            $locationIdCode = $location['geonameId'];
+            $locationIdVocab = $this->getContainer()->getParameter('geocoder')['id_vocabulary'];
 
-                if (!$geolocation) {
-                    $geolocation = new Geolocation();
-                    $geolocation->setIatiActivityId($iatiActivityId);
-                    $geolocation->setGeolocationId($locationId);
-                    $geolocation->setVocabId('99'); // TODO get a valif vocab id
-                }
-                $geolocation->setName($location['name']);
-                $geolocation->setAdminCode1Code($location['admin1']['code']);
-                $geolocation->setAdminCode1Name($location['admin1']['name']);
-                $geolocation->setAdminCode2Code($location['admin2']['code']);
-                $geolocation->setAdminCode2Name($location['admin2']['name']);
-                $geolocation->setLatitude($location['geometry']['coordinates'][0]);
-                $geolocation->setLongitude($location['geometry']['coordinates'][1]);
-                $geolocation->setExactness($location['exactness']['code']);
-                $geolocation->setClass($location['locationClass']['code']);
-                $geolocation->setDescription($location['locationClass']['description']);
-                $em->persist($geolocation);
+            $geoloc = $geolocRepo->findOneBy(array(
+                'locationIdCode' => $locationIdCode,
+                'locationIdVocab' => $locationIdVocab
+            ));
 
-                if (!$file->hasGeolocation($geolocation)) {
-                    $file->addGeolocation($geolocation);
-                }
+            if (!$geoloc) {
+                $geoloc = new Geolocation();
+                $geoloc->setName($location['toponymName']);
+                $geoloc->setLocationIdCode($locationIdCode);
+                $geoloc->setLocationIdVocab($locationIdVocab);
+                $geoloc->setFeatureLocationCode($location['fcode']);
+                $geoloc->setFeatureLocationName($location['fcodeName']);
+                $geoloc->setPointPosLat($location['lat']);
+                $geoloc->setPointPosLong($location['lng']);
+                // TODO admin
             }
+
+            $file->addGeolocation($geoloc);
         }
+
         $em->persist($file);
         $em->flush();
-    }
-
-    /**
-     * Flatten a geolocation entity instance to an associative array summarising it.
-     *
-     * @param Geolocation $location
-     * @return array see function for clearest view of keys and values
-     */
-    public function locationToArray(Geolocation $location) {
-        $data = array();
-        $data['vocab_id'] = $location->getVocabId();
-        $data['geolocation_id'] = $location->getGeolocationId();
-        $data['name'] = $location->getName();
-        $data['admin_code_1_code'] = $location->getAdminCode1Code();
-        $data['admin_code_1_name'] = $location->getAdminCode1Name();
-        $data['admin_code_2_code'] = $location->getAdminCode2Code();
-        $data['admin_code_2_name'] = $location->getAdminCode2Name();
-        $data['latitude'] = $location->getLatitude();
-        $data['longitude'] = $location->getLongitude();
-        $data['exactness'] = $location->getExactness();
-        $data['class'] = $location->getClass();
-        $data['description'] = $location->getDescription();
-        return $data;
     }
 
 }
