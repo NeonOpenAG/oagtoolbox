089aef34e1ce1c3777f85649a7cf901f05722375
Toby Batch <tobias@neontribe.co.uk>
2018-01-16 13:22:03 +0000

    Added docker is up check

--- src/OagBundle/Features/Context/FeatureContext.php
+++ src/OagBundle/Features/Context/FeatureContext.php
@@ -2,29 +2,74 @@
 
 namespace OagBundle\Features\Context;
 
+use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
 use Symfony\Component\HttpKernel\KernelInterface;
+use \Exception;
 
-class FeatureContext extends MinkContext implements KernelAwareContext
-{
+class FeatureContext extends MinkContext implements KernelAwareContext {
+
     private $kernel;
 
-    public function setKernel(KernelInterface $kernel)
-    {
+    public function setKernel(KernelInterface $kernel) {
         $this->kernel = $kernel;
     }
 
-    protected function getContainer()
-    {
+    protected function getContainer() {
         return $this->kernel->getContainer();
     }
 
     /**
      * @Given /^I wait for (\d+) seconds$/
      */
-    public function iWaitForSeconds($seconds)
-    {
+    public function iWaitForSeconds($seconds) {
         $this->getSession()->wait($seconds * 1000);
     }
+
+    /**
+     * @Given I am in the group docker
+     */
+    public function iAmInTheGroupDocker() {
+        $user = get_current_user();
+        $cmd = sprintf('id -Gn "tobias"|grep -c "docker"', $user);
+        $ingroup = exec($cmd);
+        echo $ingroup . "\n";
+        if ($ingroup == "0") {
+            throw new Exception(sprintf("Command %s returned %s\n", $cmd, $ingroup));
+        }
+    }
+
+    /**
+     * @When I run :command
+     */
+    public function iRun($command) {
+        echo "\n\n$command\n\n";
+        exec($command, $output);
+        $this->output = trim(implode("\n", $output));
+    }
+
+    /**
+     * @When I run :arg1 with the following:
+     */
+    public function iRunWithTheFollowing($arg1, TableNode $table) {
+        $rows = $table->getRows();
+        foreach ($rows as $row) {
+            $this->iRun(str_replace("<DOCKER>", $row[0], $arg1));
+        }
+    }
+
+    /**
+     * @Then I should get :arg1
+     */
+    public function iShouldGet($arg1) {
+        if ((string) $arg1 !== $this->output) {
+            throw new Exception(
+            "Actual output is:\n" . $this->output
+            );
+        }
+    }
+
 }
+
+# vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
--- src/OagBundle/Features/homepage.feature
+++ src/OagBundle/Features/homepage.feature
@@ -3,9 +3,19 @@ Feature: Site is up
   As a web user
   I need to be able to see the font page and some DOM elements that are coreect.
 
+@fontpage
 Scenario: Check the front page loads.
     Given I am on the homepage
     Then the response status code should be 200
     And I should see an ".header-nav" element
     And I should see an "main" element
-    And I should see an ".section" element
+    And I should see an "section" element
+
+@frontpageupload
+Scenario: Upload a valid XML file
+    Given I am on the homepage
+    When I attach the file "src/OagBundle/XMLTestFiles/test-one-suggested-classification.xml" to "oag_file_documentName"
+    And I press "Upload"
+    Then I should see "IATI File created"
+    And I should see an ".nav-options" element
+    And I should see an "#current-file" element
